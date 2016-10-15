1ea8a09807ca1e189baff34fae7b24f1d2079592
MinoGump <minogump@hotmail.com>
2016-10-15 18:44:36 +0800

    56

--- 56-MergeIntervals/MergeIntervals.cpp
+++ 56-MergeIntervals/MergeIntervals.cpp
@@ -16,12 +16,47 @@ class Solution {
 public:
 	vector<Interval> merge(vector<Interval>&intervals) {
 		vector<Interval> result;
-		bool willCheck = true;
-		while (willCheck) {
-			
-		}
-		return result;
+    	// quick sort with start element
+        sort(intervals, 0, intervals.size()-1);
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start <= intervals[i-1].end) {
+                int less = intervals[i-1].start, more = (intervals[i].end > intervals[i-1].end) ? intervals[i].end : intervals[i-1].end;
+                cout <<  i << "  " << less <<"  " << more << endl;
+                intervals.erase(intervals.begin()+i-1, intervals.begin()+i+1);
+                intervals.insert(intervals.begin()+i-1, Interval(less, more));
+                --i;
+            }
+        }
+        return intervals;
 	}
+
+    void sort(vector<Interval>& intervals, int start, int end) {
+        if (start < end) {
+            int index = partition(intervals, start, end);
+            sort(intervals, start, index-1);
+            sort(intervals, index+1, end);
+        }
+    }
+
+    int partition(vector<Interval>& intervals, int start, int end) {
+        Interval middle = intervals[start];
+        int lastLessIndex = end+1;
+        int index = end;
+        for ( ; index > start; --index) {
+            if (intervals[index].start >= middle.start) {
+                --lastLessIndex;
+                if (lastLessIndex != index) swap(intervals[index], intervals[lastLessIndex]);
+            }
+        }
+        swap(intervals[lastLessIndex-1], intervals[start]);
+        return lastLessIndex-1;
+    }
+
+    void swap(Interval& i1, Interval& i2) {
+        Interval temp = i1;
+        i1 = i2;
+        i2 = temp;
+    }
 };
 
 int main() {
